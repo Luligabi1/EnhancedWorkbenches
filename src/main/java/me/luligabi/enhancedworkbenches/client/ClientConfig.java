05c06c68710cce29fdfcb3a405ed5ac9437268c6
unilock <unilock@fennet.rentals>
2023-12-23 10:21:27 -0500

    Use YACL 3.2.0+'s config screen autogen
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/me/luligabi/enhancedworkbenches/client/ClientConfig.java
+++ src/main/java/me/luligabi/enhancedworkbenches/client/ClientConfig.java
@@ -1,12 +1,43 @@
 package me.luligabi.enhancedworkbenches.client;
 
+import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
+import dev.isxander.yacl3.config.v2.api.SerialEntry;
+import dev.isxander.yacl3.config.v2.api.autogen.AutoGen;
+import dev.isxander.yacl3.config.v2.api.autogen.Boolean;
+import dev.isxander.yacl3.config.v2.api.autogen.IntSlider;
+import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Identifier;
+
 public class ClientConfig {
 
 
+    public static final ConfigClassHandler<ClientConfig> HANDLER = ConfigClassHandler.createBuilder(ClientConfig.class)
+            .id(new Identifier("enhancedworkbenches", "client"))
+            .serializer(config -> GsonConfigSerializerBuilder.create(config)
+                    .setPath(FabricLoader.getInstance().getConfigDir().resolve("projecttablemod-client.json"))
+                    .build())
+            .build();
+
+    @AutoGen(category = "rendering", group = "rendering")
+    @Boolean(formatter = Boolean.Formatter.YES_NO, colored = true)
+    @SerialEntry
     public boolean renderInput = true;
+    @AutoGen(category = "rendering", group = "rendering")
+    @IntSlider(min = 1, max = 48, step = 1)
+    @SerialEntry
     public int renderInputDistance = 3;
+    @AutoGen(category = "rendering", group = "rendering")
+    @Boolean(formatter = Boolean.Formatter.YES_NO, colored = true)
+    @SerialEntry
     public boolean renderInputRequireFancy = true;
 
+    @AutoGen(category = "rendering", group = "project_table")
+    @Boolean(formatter = Boolean.Formatter.YES_NO, colored = true)
+    @SerialEntry
     public boolean renderInputOnProjectTable = true;
+    @AutoGen(category = "rendering", group = "crafting_station")
+    @Boolean(formatter = Boolean.Formatter.YES_NO, colored = true)
+    @SerialEntry
     public boolean renderInputOnCraftingStation = true;
 }
