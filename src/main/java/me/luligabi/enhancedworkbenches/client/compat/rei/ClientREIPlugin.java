88355da2ebe39cadac7eb21d9077d3817d6a0296
unilock <unilock@fennet.rentals>
2023-12-24 09:06:48 -0500

    Fix REI compat for Project Table
    A ScreenHandler should not implement Inventory lol
    
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/me/luligabi/enhancedworkbenches/client/compat/rei/ClientREIPlugin.java
+++ src/main/java/me/luligabi/enhancedworkbenches/client/compat/rei/ClientREIPlugin.java
@@ -7,9 +7,11 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.transfer.TransferHandlerRegistry;
 import me.shedaniel.rei.api.client.registry.transfer.simple.SimpleTransferHandler;
+import me.shedaniel.rei.api.common.transfer.info.stack.SlotAccessor;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.common.BuiltinPlugin;
-import net.minecraft.inventory.Inventory;
+
+import java.util.stream.IntStream;
 
 public class ClientREIPlugin implements REIClientPlugin {
 
@@ -27,11 +29,31 @@ public class ClientREIPlugin implements REIClientPlugin {
                 BuiltinPlugin.CRAFTING,
                 new SimpleTransferHandler.IntRange(1, 10)
         ));
-        registry.register(SimpleTransferHandler.create(
-                ProjectTableScreenHandler.class,
-                BuiltinPlugin.CRAFTING,
-                new SimpleTransferHandler.IntRange(1, 10),
-                new SimpleTransferHandler.IntRange(10, 64)
-        ));
+        registry.register(new SimpleTransferHandler() {
+            @Override
+            public ApplicabilityResult checkApplicable(Context context) {
+                if (!(context.getMenu() instanceof ProjectTableScreenHandler)
+                        || !BuiltinPlugin.CRAFTING.equals(context.getDisplay().getCategoryIdentifier())
+                        || context.getContainerScreen() == null) {
+                    return ApplicabilityResult.createNotApplicable();
+                } else {
+                    return ApplicabilityResult.createApplicable();
+                }
+            }
+
+            @Override
+            public Iterable<SlotAccessor> getInputSlots(Context context) {
+                return IntStream.range(1, 10)
+                        .mapToObj(id -> SlotAccessor.fromSlot(context.getMenu().getSlot(id)))
+                        .toList();
+            }
+
+            @Override
+            public Iterable<SlotAccessor> getInventorySlots(Context context) {
+                return IntStream.range(10, 64)
+                        .mapToObj(id -> SlotAccessor.fromSlot(context.getMenu().getSlot(id)))
+                        .toList();
+            }
+        });
     }
 }
